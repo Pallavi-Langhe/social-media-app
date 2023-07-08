668efe971eaa1cbec540222e421ae3e24e1f4858
Prathmesh-Dhatrak <prathmesh101dhatrak@gmail.com>
2023-07-08 16:04:21 +0530

    fix: project setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import Sidebar from '../components/Home/Sidebar';
-import Navbar from '../components/Home/Navbar';
-import Suggestions from '../components/Home/Suggestions';
-import Post from '../components/Home/Post';
+import React, { useCallback, useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { isExpired, decodeToken } from "react-jwt";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import Suggestions from "../components/Suggestions";
 
+const Home = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const authenticateUser = useCallback(async () => {
+    const encodedToken = localStorage.getItem("encoded-token");
+    if (encodedToken) {
+      const decodedToken = decodeToken(encodedToken);
+      const verifiedToken = isExpired(encodedToken);
+      if (decodedToken && verifiedToken) {
+        localStorage.setItem("user-id", decodedToken._id);
+      } else {
+        if (location.pathname !== "/signup") {
+          navigate("/login");
+        }
+      }
+    } else {
+      if (location.pathname !== "/signup") {
+        // because /signup page can be accessed without user login
+        navigate("/login");
+      }
+    }
+  }, [location.pathname, navigate]);
 
-function Home() {
-    const [userDetails, setUserDetails] = useState();
+  useEffect(() => {
+    authenticateUser();
+  }, [authenticateUser]);
 
+  return (
+    <div className="flex flex-col">
+      <Header />
+      <div className="flex justify-between">
+        <Sidebar />
+        <Outlet />
+        {location.pathname !== "/profile" &&
+          location.pathname !== "/settings" && <Suggestions />}
+      </div>
+    </div>
+  );
+};
 
-    useEffect(() => {
-        const user = localStorage.getItem("user-id");
-        setUserDetails(user);
-    }, []);
-    return (
-        <div className='flex flex-col'>
-            <Navbar />
-            <div className='flex justify-between'>
-                <Sidebar />
-                <Post />
-                <Suggestions />
-                
-            </div>
-            {userDetails}
-        </div>
-    )
-}
-
-export default Home
+export default Home;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,69 +1,26 @@
-import React, { useCallback, useEffect } from "react";
-import { Routes as AppRoutes, Route } from 'react-router-dom';
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import { isExpired, decodeToken } from "react-jwt";
-import Home from '../pages/Home'
-import Login from '../pages/Login'
-import Signup from '../pages/Signup'
-import { useState } from "react";
-// import RootLayout from '../layouts/RootLayout';
-// import UserProfile from '../pages/UserProfile';
-
-
-
-const RootLayout = () => {
-
-    const location = useLocation();
-    const navigate = useNavigate();
-    
-    const authenticateUser = useCallback(async () => {
-        const encodedToken = localStorage.getItem("encoded-token");
-        if (encodedToken) {
-            const decodedToken = decodeToken(encodedToken);
-            const verifiedToken = isExpired(encodedToken);
-            if (decodedToken && verifiedToken) {
-                localStorage.setItem('user-id', decodedToken._id);
-            } else {
-                if(location.pathname !== '/signup'){
-                    navigate("/login");
-                }
-            }
-        } else {
-            if(location.pathname !== '/signup'){ // because signup page can be accessed without user login
-                navigate("/login");
-            }
-        }
-    }, [location.pathname, navigate]);
-    
-    useEffect(() => {
-        authenticateUser();
-    }, [authenticateUser]);
-    
-    return (
-        <>
-            <Outlet />
-        </>
-    );
-};
-
-
+import { Routes as AppRoutes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Post from "../components/Post";
+import Explore from "../pages/Explore";
+import UserProfile from "../pages/UserProfile";
+import Settings from "../pages/Settings";
+import Home from "../pages/Home";
 
 function Routes() {
-    return (
-        <AppRoutes>
-            <Route path="/" Component={RootLayout}>
-                <Route
-                    index
-                    element={<Home />}
-                />
-                <Route
-                    path="/login"
-                    element={<Login />}
-                />
-                <Route path="/signup" element={<Signup />} />
-            </Route>
-        </AppRoutes>
-    );
+  return (
+    <AppRoutes>
+      <Route path="/" element={<Home />}>
+        <Route index element={<Post />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/settings" element={<Settings />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </AppRoutes>
+  );
 }
 
 export default Routes;
+
